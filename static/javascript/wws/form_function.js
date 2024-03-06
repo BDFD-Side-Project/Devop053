1025b76f22ab46f664739de04a3c1637f18f1dd9
David Chen <bdfd2005@gmail.com>
2024-03-06 10:56:57 -0500

    add form function

--- static/javascript/wws/form_function.js
+++ static/javascript/wws/form_function.js
@@ -2,7 +2,7 @@
  * @Date         : 2024-03-05 18:42:12
  * @Author       : BDFD,bdfd2005@gmail.com
  * @Github       : https://github.com/bdfd
- * @LastEditTime : 2024-03-06 09:45:27
+ * @LastEditTime : 2024-03-06 10:55:06
  * @LastEditors  : <BDFD>
  * @Description  :
  * @FilePath     : \static\javascript\wws\form_function.js
@@ -22,29 +22,43 @@ theform.o2[1].addEventListener("click", function () {
 		theform.beta.disabled = true;
 		// theform.slc.value = "";
 		theform.slc.disabled = true;
-		theform.o5[1].addEventListener("click", function () {
-			if (this.checked) {
-				theform.o4[0].disabled = true;
-				theform.o4[1].disabled = true;
-				// theform.ssd.value = "";
-				theform.ssd.disabled = true;
-				// theform.ss.value = "";
-				theform.ss.disabled = true;
-				// theform.Ksb.value = "";
-				theform.Ksb.disabled = true;
-				// theform.sy.value = "";
-				theform.sy.disabled = true;
-			}
-		});
-		theform.o5[0].addEventListener("click", function () {
-			if (this.checked) {
-				theform.o4[0].disabled = false;
-				theform.o4[1].disabled = false;
-				theform.ssd.disabled = false;
-				theform.ss.disabled = false;
-				theform.Ksb.disabled = false;
-				theform.sy.disabled = false;
-			}
-		});
+		theform.o4[0].disabled = true;
+		theform.o4[1].disabled = true;
+		theform.ssd.disabled = true;
+		theform.ss.disabled = true;
+		theform.Ksb.disabled = true;
+		theform.sy.disabled = true;
+	}
+});
+theform.o5[0].addEventListener("click", function () {
+	if (this.checked && theform.o2[0].checked) {
+		theform.o4[0].disabled = false;
+		theform.o4[1].disabled = false;
+		theform.ssd.disabled = false;
+		theform.ss.disabled = false;
+		theform.Ksb.disabled = false;
+		theform.sy.disabled = false;
+	} else {
+		theform.o4[0].disabled = true;
+		theform.o4[1].disabled = true;
+		theform.ssd.disabled = true;
+		theform.ss.disabled = true;
+		theform.Ksb.disabled = true;
+		theform.sy.disabled = true;
+	}
+});
+
+theform.o5[1].addEventListener("click", function () {
+	if (this.checked) {
+		theform.o4[0].disabled = true;
+		theform.o4[1].disabled = true;
+		// theform.ssd.value = "";
+		theform.ssd.disabled = true;
+		// theform.ss.value = "";
+		theform.ss.disabled = true;
+		// theform.Ksb.value = "";
+		theform.Ksb.disabled = true;
+		// theform.sy.value = "";
+		theform.sy.disabled = true;
 	}
 });
