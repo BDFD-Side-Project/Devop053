03ff26885c93f1c54687f4a6b21cf79bba22fd7c
David Chen <bdfd2005@gmail.com>
2024-03-26 17:36:07 -0400

    add o2, o4, o5 validataion

--- static/javascript/windwave/form_validation.js
+++ static/javascript/windwave/form_validation.js
@@ -2,7 +2,7 @@
  * @Date         : 2022-12-12 12:31:04
  * @Author       : BDFD,bdfd2005@gmail.com
  * @Github       : https://github.com/bdfd
- * @LastEditTime : 2024-03-26 17:01:56
+ * @LastEditTime : 2024-03-26 17:33:20
  * @LastEditors  : <BDFD>
  * @Description  :
  * @FilePath     : \static\javascript\windwave\form_validation.js
@@ -105,11 +105,21 @@ function validateForm() {
 			return false;
 	}
 
+	let o2 = theform.o2.value;
+	switch (true) {
+		case o2 != 1 && o2 != 2 && o2 != 3:
+			alert("请选择是否计算风壅增水。");
+			document.WWSForm.o2[0].focus();
+			return false;
+	}
+
 	if (o2 == 1) {
 		// let beta = theform.beta.value;
 		// switch (true) {
 		// 	case beta == "" || beta.trim() == "":
-		// 		break;
+		// 		alert("请在此输入数值。");
+		// 		document.WWSForm.beta.focus();
+		// 		return false;
 		// 	case !beta.match(pos_real_number):
 		// 		alert("请在此输入数值。");
 		// 		document.WWSForm.beta.focus();
@@ -119,7 +129,9 @@ function validateForm() {
 		// let slc = theform.slc.value;
 		// switch (true) {
 		// 	case slc == "" || slc.trim() == "":
-		// 		break;
+		// 		alert("请在此输入数值。");
+		// 		document.WWSForm.slc.focus();
+		// 		return false;
 		// 	case !slc.match(pos_real_number):
 		// 		alert("请在此输入数值。");
 		// 		document.WWSForm.slc.focus();
@@ -127,7 +139,21 @@ function validateForm() {
 		// }
 
 		if (o1 == 1) {
+			let o5 = theform.o5.value;
+			switch (true) {
+				case o5 != 1 && o5 != 2 && o5 != 3:
+					alert("请选择是否有水深剖面数据。");
+					document.WWSForm.o5[0].focus();
+					return false;
+			}
 			if (o5 == 1) {
+				let o4 = theform.o4.value;
+				switch (true) {
+					case o4 != 1 && o4 != 2 && o4 != 3:
+						alert("请选择水域底坡特点。");
+						document.WWSForm.o4[0].focus();
+						return false;
+				}
 				let xs = theform.xs.value;
 				switch (true) {
 					case xs == "" || xs.trim() == "":
@@ -173,7 +199,7 @@ function validateForm() {
 					case xlook.trim() == "":
 						// alert("xlook 输入为多个空格,作为None输出");
 						break;
-					case !xlook.match(pos_real_number) && parseFloat(xlook) <= 0:
+					case !xlook.match(pos_real_number) && parseFloat(xlook) > 0:
 						alert("无法在基点的岸侧查看风壅增水。");
 						document.WWSForm.xlook.focus();
 						return false;
